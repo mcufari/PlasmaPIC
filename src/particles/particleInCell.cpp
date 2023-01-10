980c8cf52beec220944d267f35fd759163213019
mcufari <mcufari@syr.edu>
2023-01-09 21:06:13 -0500

    Changes to particle initialization routine

--- src/particles/particleInCell.cpp
+++ src/particles/particleInCell.cpp
@@ -16,7 +16,7 @@ void Grid<1>::particleInCell(const Particle<1>* pList, const int NParts) {
 
 void Grid<1>::particleInitRandomStaticProton(Particle<1>* pList, const int NParts) const {
     for(int i = 0; i < NParts; i++){
-        pList[i] = Particle<1>(1,0.15,0,0.0001);
+        pList[i] = Particle<1>(1,0.15,0,0.01);
    
         pList[i].velocity = 0;
       
@@ -31,11 +31,11 @@ void Grid<1>::particleInitStaticProtonPair(Particle<1>* pList, const int NParts)
         exit(1);
     }
     for(int i = 0; i < NParts/2; i++){
-        pList[i] = Particle<1>(-1,lBound + dx + ((rBound-dx) - (lBound + dx))*((double)rand())/RAND_MAX, 0, 0.0001);
+        pList[i] = Particle<1>(-1,lBound + dx + ((rBound-dx) - (lBound + dx))*((double)rand())/RAND_MAX, 0, 0.01);
         //std::cout << "Placing particle at: " << pList[i].position << std::endl;
     }
     for(int i = NParts/2; i < NParts; i++){
-        pList[i] = Particle<1>(1, lBound + dx + ((rBound-dx) - (lBound + dx))*((double)rand())/RAND_MAX, 0 ,0.0001);
+        pList[i] = Particle<1>(1, lBound + dx + ((rBound-dx) - (lBound + dx))*((double)rand())/RAND_MAX, 0 ,0.01);
         //std::cout << "Placing particle at: " << pList[i].position << std::endl;
     }
     
@@ -44,7 +44,7 @@ void Grid<1>::particleInitStaticProtonPair(Particle<1>* pList, const int NParts)
 
 void Grid<1>::particleInitUniformProtonElectronPairs(Particle<1>* pList, const int NParts){
     for(int i = 0; i < NParts; i+=2){
-        pList[i] = Particle<1>(-1, lBound + dx + ((rBound-dx) - (lBound + dx)) * i / NParts, 0, 0.0001);
+        pList[i] = Particle<1>(-1, lBound + dx + ((rBound-dx) - (lBound + dx)) * i / NParts, 0, 0.01);
     }
     for(int i = 1; i < NParts; i += 2){
         pList[i] = Particle<1>(1, lBound + dx + ((rBound-dx) - (lBound + dx)) * i / NParts, 0, 0.01);
