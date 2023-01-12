e048a7809767af90c7e9b9cdade5d841791cc265
mcufari <mcufari@syr.edu>
2023-01-12 00:32:05 -0500

    wip

--- src/particles/Particle.cpp
+++ src/particles/Particle.cpp
@@ -4,10 +4,23 @@ Particle<1>::Particle(double ch, double pos, double vel, double mass):
     charge(ch), 
     position(pos),
     velocity(vel),
-    mass(mass)
-{}
+    mass(mass),
+    qmRatio(ch/mass)
+{
+    lIndex  = 0;
+    rIndex =  0;
+}
 
-void Particle<1>::cloudInCell(double* lhCharge, double* rhCharge, double* lhs, double* rhs, const double dx) const {
-   *lhCharge += this->charge * ((*rhs) - this->position)/dx;
-    *rhCharge += this->charge * (this->position - (*lhs))/dx;
+void Particle<1>::cloudInCell(double* lhCharge, double* rhCharge, double lhs, double rhs, const double rdx) const {
+   
+   *lhCharge += charge * ((rhs) - position) * rdx;
+    *rhCharge += charge * (position - (lhs)) * rdx;
+}
+
+Particle<1>::Particle(const Particle<1>& prhs){
+    mass = prhs.mass;
+    charge = prhs.charge;
+    velocity = prhs.velocity;
+    position = prhs.position;
+    qmRatio = charge/mass;
 }
