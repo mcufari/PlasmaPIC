e6c864ea3e3d3a6c4614d1aaa9340afafc8d13aa
mcufari <mcufari@syr.edu>
2023-01-12 00:32:10 -0500

    wip

--- src/particles/Particle.hpp
+++ src/particles/Particle.hpp
@@ -12,11 +12,21 @@ public:
 template<> class Particle<1>{
 public:
     Particle(double ch, double pos, double vel, double mass);
-    void cloudInCell(double* lhCharge, double* rhCharge, double* lhs, double* rhs, const double dx) const;
+    Particle(const Particle<1>& prhs);
+    void cloudInCell(double* lhCharge, double* rhCharge, double lhs, double rhs, const double rdx) const;
+    
     double charge;
     double position;
+    double oldPosition;
     double velocity;
     double mass;
+    double qmRatio;
+
+    int lIndex;
+    int rIndex;
+
+    double vely;
+    double velz;
 
 };
 
